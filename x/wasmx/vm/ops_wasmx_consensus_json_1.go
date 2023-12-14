9fa62123033c8199b0445e37d90d22fdf255b0e9
Loredana Cirstea <loredana.cirstea@gmail.com>
2023-12-14 19:22:24 +0100

    add Logger functions for wasmx consensus host standard

--- x/wasmx/vm/ops_wasmx_consensus_json_1.go
+++ x/wasmx/vm/ops_wasmx_consensus_json_1.go
@@ -1,8 +1,6 @@
 package vm
 
 import (
-	"encoding/base64"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 
@@ -28,7 +26,6 @@ func merkleHash(_context interface{}, callframe *wasmedge.CallingFrame, params [
 		return nil, wasmedge.Result_Fail
 	}
 	hashbz := merkle.HashFromByteSlices(val.Slices)
-
 	ptr, err := allocateWriteMem(ctx, callframe, hashbz)
 	if err != nil {
 		return nil, wasmedge.Result_Fail
@@ -187,24 +184,88 @@ func CheckTx(_context interface{}, callframe *wasmedge.CallingFrame, params []in
 	return returns, wasmedge.Result_Success
 }
 
+type LoggerLog struct {
+	Msg   string   `json:"msg"`
+	Parts []string `json:"parts"`
+}
+
+func getLoggerData(callframe *wasmedge.CallingFrame, params []interface{}) (string, []any, error) {
+	message, err := readMemFromPtr(callframe, params[0])
+	if err != nil {
+		return "", nil, err
+	}
+	var data LoggerLog
+	err = json.Unmarshal(message, &data)
+	if err != nil {
+		return "", nil, err
+	}
+	parts := make([]any, len(data.Parts))
+	for i, part := range data.Parts {
+		parts[i] = part
+	}
+	return data.Msg, parts, nil
+}
+
+func wasmxLoggerInfo(context interface{}, callframe *wasmedge.CallingFrame, params []interface{}) ([]interface{}, wasmedge.Result) {
+	ctx := context.(*Context)
+	msg, parts, err := getLoggerData(callframe, params)
+	if err != nil {
+		return nil, wasmedge.Result_Fail
+	}
+	ctx.GetContext().Logger().Info(msg, parts...)
+	returns := make([]interface{}, 0)
+	return returns, wasmedge.Result_Success
+}
+
+func wasmxLoggerError(context interface{}, callframe *wasmedge.CallingFrame, params []interface{}) ([]interface{}, wasmedge.Result) {
+	ctx := context.(*Context)
+	msg, parts, err := getLoggerData(callframe, params)
+	if err != nil {
+		return nil, wasmedge.Result_Fail
+	}
+	ctx.GetContext().Logger().Error(msg, parts...)
+	returns := make([]interface{}, 0)
+	return returns, wasmedge.Result_Success
+}
+
+func wasmxLoggerDebug(context interface{}, callframe *wasmedge.CallingFrame, params []interface{}) ([]interface{}, wasmedge.Result) {
+	ctx := context.(*Context)
+	msg, parts, err := getLoggerData(callframe, params)
+	if err != nil {
+		return nil, wasmedge.Result_Fail
+	}
+	// TODO Debug!
+	ctx.GetContext().Logger().Info(msg, parts...)
+	returns := make([]interface{}, 0)
+	return returns, wasmedge.Result_Success
+}
+
 func BuildWasmxConsensusJson1(context *Context) *wasmedge.Module {
 	env := wasmedge.NewModule("consensus")
 	functype__i32 := wasmedge.NewFunctionType(
 		[]wasmedge.ValType{},
 		[]wasmedge.ValType{wasmedge.ValType_I32},
 	)
+	functype_i32_ := wasmedge.NewFunctionType(
+		[]wasmedge.ValType{wasmedge.ValType_I32},
+		[]wasmedge.ValType{},
+	)
 	functype_i32_i32 := wasmedge.NewFunctionType(
 		[]wasmedge.ValType{wasmedge.ValType_I32},
 		[]wasmedge.ValType{wasmedge.ValType_I32},
 	)
-
-	env.AddFunction("MerkleHash", wasmedge.NewFunction(functype_i32_i32, merkleHash, context, 0))
 
 	env.AddFunction("PrepareProposal", wasmedge.NewFunction(functype_i32_i32, PrepareProposal, context, 0))
 	env.AddFunction("ProcessProposal", wasmedge.NewFunction(functype_i32_i32, ProcessProposal, context, 0))
 	env.AddFunction("FinalizeBlock", wasmedge.NewFunction(functype_i32_i32, FinalizeBlock, context, 0))
 	env.AddFunction("Commit", wasmedge.NewFunction(functype__i32, Commit, context, 0))
 	env.AddFunction("CheckTx", wasmedge.NewFunction(functype_i32_i32, CheckTx, context, 0))
+
+	env.AddFunction("MerkleHash", wasmedge.NewFunction(functype_i32_i32, merkleHash, context, 0))
+
+	env.AddFunction("LoggerInfo", wasmedge.NewFunction(functype_i32_, wasmxLoggerInfo, context, 0))
+	env.AddFunction("LoggerError", wasmedge.NewFunction(functype_i32_, wasmxLoggerError, context, 0))
+	env.AddFunction("LoggerDebug", wasmedge.NewFunction(functype_i32_, wasmxLoggerDebug, context, 0))
 
 	return env
 }
