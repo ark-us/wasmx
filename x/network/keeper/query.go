af8f3175c3da4d0e0ec3b4e802afed309ecfe521
Loredana Cirstea <loredana.cirstea@gmail.com>
2024-04-16 18:38:15 +0200

    remove outdated params query for network module

--- x/network/keeper/query.go
+++ x/network/keeper/query.go
@@ -1,23 +1,7 @@
 package keeper
 
 import (
-	"context"
-
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
-	sdk "github.com/cosmos/cosmos-sdk/types"
-
 	"mythos/v1/x/network/types"
 )
 
 var _ types.QueryServer = &Keeper{}
-
-func (k *Keeper) Params(goCtx context.Context, req *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
-	}
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	return &types.QueryParamsResponse{Params: k.GetParams(ctx)}, nil
-}
